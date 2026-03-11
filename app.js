f3b6890e8c97a84af68313ecaa42a5d9562af2db
esyauliatria <esyaulia300@gmail.com>
2026-03-11 18:39:53 +0700

    modul 5 dan 6

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var flash = require('express-flash');
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -23,6 +26,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  cookie: {
+    maxAge: 6000
+  },
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
+}))
+
+app.use(flash())
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/biodata.js
+++ routes/biodata.js
@@ -1,10 +1,17 @@
 var express = require('express');
+const connection = require('../config/database');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('biodata',
-    {
-        judul : 'TABEL BIODATA'
+    connection.query('select * from biodata order by id_biodata desc', function(err,rows){
+        if(err){
+            req.flash('error', err);
+        }else{
+            res.render('biodata',{
+                judul : 'Halaman Biodata',
+                data: rows
+            });
+        }
     });
 });
 
